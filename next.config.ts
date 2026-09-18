371f5c35cb805eee37b860adf26cb70ad7c82a5d
Vishvjeet <apple@Apples-MacBook-Air-2.local>
2026-09-18 19:32:57 +0530

    Redirect root to dashboard

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
